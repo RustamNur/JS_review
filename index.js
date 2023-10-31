214b137e42354f533d5ab8b22c4e5924f729ae62
RustamNur <rustam161723@gmail.com>
2023-10-31 14:51:01 +0300

    review-2

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 //review-2
+
+//Switch/Case
+// switch as IF statement
+// allow only exact value
+
+//(Uzbek lang)
+//  (! - 1) Bir qiymatni bir nechta variantlar bilan 'solishtirish' uchun ishlatamiz
+
+// (! - 2) syntax clean , strict checking
+
+// (! - 3) caselardagi variantlar false qiymatda bolsa defaultga qaytadi
+
+// (! - 4) 'break' keywordini ishlatmasak trueni topgandan keyin ham boshqa kodlarni yurguzib yuboradi
+
+// (! - 5) IF = imprecise(impresays) -> noaniq qiymat
+// SWITCH = precise (presays) -> aniq qiymat
+
+// (! -6) IF CAN use 'logical' and 'comparison' operators
+// SWITCH/CASE must be assignable, CAN'T use 'logical' and 'comparison' operators
+
+// syntax
+switch (variable) {
+  case "value of variable":
+    console.log("COde");
+    break;
+  case "value of variable":
+    console.log("Code");
+    break;
+  default:
+    console.log("Another code");
+}
+
+// Checking => Read and return only strict equality
+//
